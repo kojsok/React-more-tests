6405d955d250d550f1820d45856f9ba395c9ab3b
Art R <pmbiclub@gmail.com>
2024-09-06 13:57:45 +0300

    add all

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css'
 import AxiosAndRouting from './components/axiosAndRouting/AxiosAndRouting';
+import { Loader } from './components/Loader/Loader';
 
 
 function App() {
 
   return (
     <div className="app">
+      <Loader classList={['loader-centern']} />
       <AxiosAndRouting />
+      
     </div>
   )
 }
